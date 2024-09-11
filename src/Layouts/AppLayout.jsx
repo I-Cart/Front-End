d59c76178c442295e5e07958262cc1d30b8f6100
Ahmed El-Sayad <ahmed.ehab.elsayad@gmail.com>
2024-09-11 23:06:17 +0300

    updating layout

--- src/Layouts/AppLayout.jsx
+++ src/Layouts/AppLayout.jsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 function AppLayout() {
-  return <Outlet />;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Outlet />
+    </div>
+  );
 }
 
 export default AppLayout;
